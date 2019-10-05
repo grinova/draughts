0dd77d83fc41ab42cba8a465efebe2721d0a14da
Aleksandr Kuzmin <grinova@gmail.com>
2019-10-05 17:54:06 +0300

    Implement endGame

--- src/server/game/end-game.js
+++ src/server/game/end-game.js
@@ -1,4 +1,22 @@
+const Vec2 = require('../../common/vec2')
+const possibleMovePositions = require('../../common/game/possible-move-positions')
+
 function endGame(field) {
+  let white = 0
+  let black = 0
+  for (let row = 0; row < 8; row++) {
+    for (let column = 0; column < 8; column++) {
+      const piece = field[row][column]
+      if (piece == 'w' || piece == 'wk') {
+        white += possibleMovePositions(new Vec2(column, row), field)
+      } else if (piece == 'b' || piece == 'bk') {
+        black += possibleMovePositions(new Vec2(column, row), field)
+      }
+    }
+  }
+  if (white == 0 || black == 0) {
+    return { isEnd: true, winner: white > 0 ? 0 : 1 }
+  }
   return { isEnd: false }
 }
 
