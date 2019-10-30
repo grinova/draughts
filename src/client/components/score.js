eb015b8f3239d319d974f8cfa0d376b23bfb3ef1
Aleksandr Kuzmin <grinova@gmail.com>
2019-10-30 14:05:25 +0300

    Fix score board

--- src/client/components/score.js
+++ src/client/components/score.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 import Piece from './piece'
+import { SCORE_SIZE } from '../config'
 import { range } from '../common/fn'
 
 const PlayerName = styled.h3`
@@ -9,8 +10,8 @@ const PlayerName = styled.h3`
 
 const Taken = styled.div`
   display: grid;
-  grid-template-rows: 1fr 1fr 1fr;
-  grid-template-columns: 1fr 1fr 1fr 1fr;
+  grid-template-rows: ${_ => range(SCORE_SIZE.height).map(_ => '1fr').join(' ')};
+  grid-template-columns: ${_ => range(SCORE_SIZE.width).map(_ => '1fr').join(' ')};
   margin: 10px;
   grid-gap: 10px;
   justify-content: center;
@@ -22,6 +23,10 @@ const SmallPiece = styled(Piece)`
   height: 5vmin;
 `
 
+const HidenSmallPiece = styled(SmallPiece)`
+  visibility: hidden;
+`
+
 const Score = ({ className, playerName, score, side }) => {
   return (
     <div className={className}>
@@ -29,6 +34,9 @@ const Score = ({ className, playerName, score, side }) => {
       <Taken>
         {range(score).map(i => (
           <SmallPiece key={i} piece={side}/>
+        ))}
+        {range(SCORE_SIZE.width * SCORE_SIZE.height - score).map(i => (
+          <HidenSmallPiece key={score + i} piece={side}/>
         ))}
       </Taken>
     </div>
--- src/client/config.js
+++ src/client/config.js
@@ -2,3 +2,8 @@ export const ROUTES = {
   HOME: '/',
   BOARD: '/board'
 }
+
+export const SCORE_SIZE = {
+  width: 4,
+  height: 3
+}
